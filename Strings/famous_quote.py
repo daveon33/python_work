6fdd491be6633f624e485b886eba0e30ecfd0ba4
David <davidgonzalez5910@gmail.com>
2025-06-12 17:17:40 -0500

    Comments Ex 1

--- Strings/famous_quote.py
+++ Strings/famous_quote.py
@@ -1,2 +1,4 @@
+# June 12
+
 quote = "La pregunta original, '¿Pueden pensar las máquinas?', me parece demasiado insignificante como para merecer debate. Sin embargo, creo que a finales del siglo XX el uso del lenguaje y la opinión general habrán cambiado tanto que se podrá hablar de máquinas pensantes sin esperar contradicción."
 print(f"Alan Turing dijo una vez '{quote}'")
