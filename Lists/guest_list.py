edce34a9e523e6c69364d561f74e91fcf6762b33
David <davidgonzalez5910@gmail.com>
2025-06-12 19:14:45 -0500

    Lists Ex 1

---
+++ Lists/guest_list.py
@@ -0,0 +1,3 @@
+guest_list = ['Dante', 'Reno', 'Tito']
+
+print(f"I would love to see you all at my party this weekend. Can't wait to see {guest_list[0]}'s dance moves, laugh with the goofy {guest_list[1]}, and enjoy a pleasant conversation with {guest_list[2]}")
