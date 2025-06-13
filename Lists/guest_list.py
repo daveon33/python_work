6085a9a5610ce13f5374af0b092b291396be3a08
David <davidgonzalez5910@gmail.com>
2025-06-12 19:39:34 -0500

    Lists Final Ex

--- Lists/guest_list.py
+++ Lists/guest_list.py
@@ -15,3 +15,28 @@ guest_list.insert(2, 'Mateo')
 guest_list.append('Daniel')
 
 print(f"I hope you can come to my party {guest_list[0]}, {guest_list[2]} and {guest_list[-1]}")
+
+print("Unfortunately there's a change in plans, now I can only invite 2 of you...")
+
+no_longer_invited = guest_list.pop()
+
+print(f"I'm very sorry but {no_longer_invited} you are no longer invited")
+
+no_longer_invited = guest_list.pop()
+
+print(f"I'm very sorry but {no_longer_invited} you are no longer invited")
+
+no_longer_invited = guest_list.pop()
+
+print(f"I'm very sorry but {no_longer_invited} you are no longer invited")
+
+no_longer_invited = guest_list.pop()
+
+print(f"I'm very sorry but {no_longer_invited} you are no longer invited")
+
+print(f"I can't wait to see {guest_list[0]} and {guest_list[1]} in my party!")
+
+del guest_list[1]
+del guest_list[0]
+
+print(guest_list)
