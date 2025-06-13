b4af9c7f72356cff81af95b47184b3e3ecc5bd72
David <davidgonzalez5910@gmail.com>
2025-06-12 19:28:40 -0500

    Lists Ex 3

--- Lists/guest_list.py
+++ Lists/guest_list.py
@@ -7,3 +7,11 @@ print(f"It seems only {guest_list[1]} is unable to attend my party")
 guest_list[1] = 'Andres'
 
 print(f"Now {guest_list[1]} is invited and he will bring some refreshments for all!")
+
+print("I have found a bigger table! Now I can invite 3 more people...")
+
+guest_list.insert(0, 'Giovanni')
+guest_list.insert(2, 'Mateo')
+guest_list.append('Daniel')
+
+print(f"I hope you can come to my party {guest_list[0]}, {guest_list[2]} and {guest_list[-1]}")
