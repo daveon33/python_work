f41320fe0b2a92566d251bb50b5afb8ffd882cf3
David <davidgonzalez5910@gmail.com>
2025-06-14 17:03:34 -0500

    Sorting List Ex 1

---
+++ Lists/seeing_the_world.py
@@ -0,0 +1,16 @@
+places_to_travel = ['greece', 'japan', 'brazil', 'italy', 'argentina']
+
+print(places_to_travel)
+print(sorted(places_to_travel))
+print(places_to_travel)
+print(sorted(places_to_travel, reverse = True))
+print(places_to_travel)
+places_to_travel.reverse()
+print(places_to_travel)
+places_to_travel.reverse()
+print(places_to_travel)
+places_to_travel.sort()
+print(places_to_travel)
+places_to_travel.sort(reverse = True)
+print(places_to_travel)
+
