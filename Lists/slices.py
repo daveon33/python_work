e9b77c16570319bf2c55e1741e85f5bb2e2a8131
David <davidgonzalez5910@gmail.com>
2025-12-28 21:32:59 -0500

    Ex 4.10

---
+++ Lists/slices.py
@@ -0,0 +1,5 @@
+three_multiples = [number for number in range(3, 31, 3)]
+
+print(f"The first three items in the list are: {three_multiples[0:3]}")
+print(f"Three items from the middle of the list are: {three_multiples[5:8]}")
+print(f"The last three items in the list are: {three_multiples[-3:]}")
