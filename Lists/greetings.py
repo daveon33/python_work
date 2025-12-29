07a3261d9b429fac31514637c29c6794d5275b98
David <davidgonzalez5910@gmail.com>
2025-12-28 20:43:36 -0500

    Ex 3.2

---
+++ Lists/greetings.py
@@ -0,0 +1,8 @@
+friends_names = ['Alejandro', 'Ricardo', 'Maria', 'Esteban', 'Tatiana']
+
+print(f"{friends_names[0]}, I think we have many things in common")
+print(f"{friends_names[1]}, your cunning is something to be afraid of")
+print(f"{friends_names[2]}, I think you're very reliable")
+print(f"{friends_names[3]}, there's no way to be bored around you")
+print(f"{friends_names[4]}, I am a bit jealous of your mind")
+
