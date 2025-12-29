c8d2fba2993e751ea7690a655932661baa9f77a3
David <davidgonzalez5910@gmail.com>
2025-12-28 21:36:23 -0500

    Ex 4.11

--- Lists/pizzas.py
+++ Lists/pizzas.py
@@ -1,6 +1,14 @@
 favorite_pizzas = ['pineapple', 'mexican', 'Chicken and mushrooms']
+friend_pizzas = favorite_pizzas[:]
+
+favorite_pizzas.append('paisa')
+friend_pizzas.append('pepperoni')
 
 for pizza in favorite_pizzas:
     print(f"I like {pizza} pizza")
 
 print("I really love pizza!")
+print(f"My favorite pizzas are: {favorite_pizzas}")
+
+for pizza in friend_pizzas:
+    print(pizza)
