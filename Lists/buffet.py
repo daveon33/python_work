e70639bbdef6149e4df8707096a53c0db89a3dec
David <davidgonzalez5910@gmail.com>
2025-12-30 20:48:21 -0500

    Ex 4.13

---
+++ Lists/buffet.py
@@ -0,0 +1,11 @@
+buffet_foods = ("Sushi", "Pancakes", "Steak", "Ice cream", "Dumplings")
+
+for food in buffet_foods:
+    print(food)
+
+# Trying to add new items to a tuple won't work
+
+buffet_foods = ("Sushi", "Pancakes", "Steak", "Ice cream", "Dumplings", "Tortilla", "Apple Pie")
+
+for food in buffet_foods:
+    print(food)
