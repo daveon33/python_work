354ef831643525bf4f7ca2b59a7645c23bae627c
David <davidgonzalez5910@gmail.com>
2026-01-02 17:37:10 -0500

    Ex 5.9

--- conditionals/hello_admin.py
+++ conditionals/hello_admin.py
@@ -6,4 +6,14 @@ for username in usernames:
     else:
         print(f"Welcome back {username}, thank you for logging in again!")
 
-    
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if(username == 'admin'):
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Welcome back {username}, thank you for logging in again!")
+
+else:
+    print("We need to find some users!")
