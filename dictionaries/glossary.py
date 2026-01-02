fa308dd041d99d177c89c806fae3865bd681529d
David <davidgonzalez5910@gmail.com>
2026-01-02 18:27:50 -0500

    Ex 6.4

---
+++ dictionaries/glossary.py
@@ -0,0 +1,11 @@
+programming_words = {
+    "conditional": "Checks if the statement is true or false",
+    "loop": "Repeats the instruction",
+    "string": "A concatenation of characters",
+    "c++": "Programming language",
+    "dictionary": "Collection of key-value pairs",
+
+}
+
+for word, meaning in programming_words.items():
+    print(f"{word} {meaning} \n")
