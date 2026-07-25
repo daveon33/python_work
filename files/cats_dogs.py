a7db547825d1031ded0b17e3af286eaba8cd46c5
David <davidgonzalez5910@gmail.com>
2026-07-25 17:55:51 -0500

    Ex 10.8

---
+++ files/cats_dogs.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+files = ['files/text_files/cats.txt', 'files/text_files/dogs.txt']
+
+for file in files:
+    try:
+        path = Path(file)
+        print(path.read_text())
+    except FileNotFoundError:
+        print(f"The file {file} was not found!")
+
+
