2c7ed39f88c3c9f5ecd2621efe9da74eae0227a3
David <davidgonzalez5910@gmail.com>
2026-07-25 17:33:09 -0500

    Ex 10.7

--- files/addition.py
+++ files/addition.py
@@ -1,9 +1,15 @@
-number_one = input('What\'s the first number? ')
-number_two = input('What\'s the second number? ')
+while(True):
+    number_one = input('What\'s the first number? ')
+    number_two = input('What\'s the second number? ')
+    try:
+        addition = int(number_one) + int(number_two)
+    except ValueError:
+        print("At least one of the inputs was not a number, try again")
+    else:
+        print(f"The sum is {addition}")
+        finishing = input("Are you finished? (Y/N) ")
 
-try:
-    addition = int(number_one) + int(number_two)
-except ValueError:
-    print("At least one of the inputs was not a number, try again")
-else:
-    print(f"The sum is {addition}")
+        if finishing == "Y" or finishing == "y":
+            break
+        else:
+            continue
