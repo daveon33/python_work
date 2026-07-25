3e0fd3ccd9d5b3bbd2b2fa1e502b59e3af7d1471
David <davidgonzalez5910@gmail.com>
2026-07-25 16:57:41 -0500

    Ex 10.5

--- files/guest.py
+++ files/guest.py
@@ -1,5 +1,16 @@
 from pathlib import Path
 
 path = Path('files\guest.txt')
-new_guest = input('What is your name? ')
-path.write_text(new_guest)
+our_guests = ""
+
+while(True):
+    new_guest = input('What is your name? ')
+    our_guests += new_guest + '\n'    
+    guest_remaining = input('Are there any more guests? (Y/N): ')
+
+    if guest_remaining == "Y" or guest_remaining == "y":
+        continue
+    else:
+        break
+
+path.write_text(our_guests)
