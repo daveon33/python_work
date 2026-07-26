424e07aa6f4732d0a7a94fbc9d57036844c3bcbb
David <davidgonzalez5910@gmail.com>
2026-07-26 13:25:32 -0500

    Ex 10.14

--- files/remember_me.py
+++ files/remember_me.py
@@ -29,8 +29,12 @@ def greet_user():
     """Greet the user by name."""
     path = Path('username.json')
     my_user = get_stored_user(path)
+    verify_user = input(f"Is this your username? (Y/N): {my_user["username"]} ")
+    if verify_user == "N" or verify_user == "n":
+        my_user = get_new_user(path)
+        
     if my_user:
-        print(f"Welcome back, {my_user["username"]}. Your age is my_user["age"]}, and your favorite programming language is {my_user["favorite language"]}!")
+        print(f"Welcome back, {my_user["username"]}. Your age is {my_user["age"]}, and your favorite programming language is {my_user["favorite language"]}!")
     else:
         my_user = get_new_user(path)
         print(f"We'll remember you when you come back, {my_user['username']}!")
