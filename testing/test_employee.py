0d8f8a7a590a69e2da3073cab641cf0ddc62dd63
David <davidgonzalez5910@gmail.com>
2026-07-26 17:00:22 -0500

    Ex 11.3 W/ Fixture

--- testing/test_employee.py
+++ testing/test_employee.py
@@ -1,14 +1,16 @@
+import pytest
 from employee import Employee
 
-
-
-def test_give_default_raise():
+@pytest.fixture
+def new_employee():
     new_employee = Employee('David', 'Gallego', 20000000)
+    return new_employee
+
+def test_give_default_raise(new_employee):
     new_employee.give_raise()
     assert new_employee.annual_salary == 20005000
 
-def test_give_custom_raise():
-    new_employee = Employee('Santiago', 'Gal', 10000000)
+def test_give_custom_raise(new_employee):
     new_employee.give_raise(50000)
-    assert new_employee.annual_salary == 10050000
+    assert new_employee.annual_salary == 20050000
 
